76ef436aac60199d502165f3b3749cbe3a0e021f
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-03 16:16:24 -0400

    televisions and computers

--- lab_1/computers.sql
+++ lab_1/computers.sql
@@ -4,9 +4,9 @@
 -- Write SQL commands under the prompts below, and run the file to get results.
 
 -- In case there is already a computers table, drop it
-
+DROP TABLE computers;
 -- Create a computers table
-
+Create table computers (id serial, make varchar(20), model varchar(20), cpu_speed decimal, memory_size decimal, price decimal, release_date timestamp, photo_url varchar(100), storage_amount decimal, number_usb_ports int, number_firewire_ports int, number_thunderbolt_ports int);
 
 -- The table should have id, make, model, cpu_speed, memory_size,
 --  price, release_date, photo_url, storage_amount, number_usb_ports,
@@ -14,10 +14,13 @@
 
 
 -- Insert 4 computers into the computers table
-
+Insert into computers (make, model, cpu_speed, memory_size, price, release_date, photo_url, storage_amount, number_usb_ports, number_firewire_ports, number_thunderbolt_ports) values ('Apple', 'Macbook 2', 123, 456, 555, '2024-06-03 15:30:00', 'https://www.absdf.come', 444, 3, 4, 5);
+Insert into computers (make, model, cpu_speed, memory_size, price, release_date, photo_url, storage_amount, number_usb_ports, number_firewire_ports, number_thunderbolt_ports) values ('Apple', 'Macbook Air', 126, 466, 455, '2023-06-03 15:30:00', 'https://www.apple.come', 434, 3, 4, 5);
+Insert into computers (make, model, cpu_speed, memory_size, price, release_date, photo_url, storage_amount, number_usb_ports, number_firewire_ports, number_thunderbolt_ports) values ('Dell', 'Notebook 2', 123, 456, 555, '2023-02-03 15:30:00', 'https://www.dell.come', 144, 2, 3, 4);
+Insert into computers (make, model, cpu_speed, memory_size, price, release_date, photo_url, storage_amount, number_usb_ports, number_firewire_ports, number_thunderbolt_ports) values ('Compaq', 'Labptop 2', 123, 456, 555, '2012-06-03 15:30:00', 'https://www.compaq.come', 124, 2, 3, 2);
 
 -- Select all entries from the computers table
-
+SELECT * from computers;
 
 -- HUNGRY FOR MORE? 
 -- Look at this afternoon's instructor notes and read on altering tables before attempting below
--- lab_1/televisions.sql
+++ lab_1/televisions.sql
@@ -4,20 +4,24 @@
 -- Write SQL commands under the prompts below, and run the file to get results.
 
 -- In case there is already a televisions table, drop it
-
+DROP TABLE televisions;
 
 -- Create a televisions table
 
 
 --  The table should have id, model_name, screen_size, resolution,
 --  price, release_date, photo_url
+Create table televisions (id serial, model_name varchar(20), screen_size decimal, resolution decimal, price decimal, release_date timestamp, photo_url varchar(100));
 
 
 -- Insert 4 televisions into the tv_models table
-
+Insert into televisions (model_name, screen_size, resolution, price, release_date, photo_url) values ('M43', 123, 456, 555, '2024-06-03 15:30:00', 'https://www.absdf.come');
+Insert into televisions (model_name, screen_size, resolution, price, release_date, photo_url) values ('XZ3', 133, 416, 155, '2023-06-03 15:30:00', 'https://www.absff.com');
+Insert into televisions (model_name, screen_size, resolution, price, release_date, photo_url) values ('PR3', 163, 466, 525, '2022-06-03 15:30:00', 'https://www.absass.com');
+Insert into televisions (model_name, screen_size, resolution, price, release_date, photo_url) values ('LQ3', 193, 436, 553, '2021-06-03 15:30:00', 'https://www.abvvvvsdf.com');
 
 -- Select all entries from the tv_models table
-
+Select * from televisions;
 
 -- HUNGRY FOR MORE? 
 -- Look at this afternoon's instructor notes and read on altering tables before attempting below
