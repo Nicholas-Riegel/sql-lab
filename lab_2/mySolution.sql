54a19f2490c29f8fede090f3fba094c82ef29f71
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-04 18:06:22 -0400

    airlines done

--- lab_2/mySolution.sql
+++ lab_2/mySolution.sql
@@ -1,11 +1,48 @@
 SELECT 
 	origin_airports.name as origin_airport, 
 	origin_airports.city as origin_city,
-	routes.origin_code
+	routes.origin_code,
+	destination_airports.name as destination_airport,
+	destination_airports.city as destination_city,
+	routes.dest_code
 from airports AS origin_airports 
 JOIN routes 
 on origin_airports.iata_faa = routes.origin_code 
-where origin_airports.city = 'New York'
 JOIN airports as destination_airports
 on destination_airports.iata_faa = routes.dest_code
-where destination_airports.city = 'Paris';
+where destination_airports.city = 'Paris' 
+and origin_airports.city = 'New York';
+  
+--   origin_airport    | origin_city | origin_code | destination_airport | destination_city | dest_code 
+-- ---------------------+-------------+-------------+---------------------+------------------+-----------
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Charles De Gaulle   | Paris            | CDG
+--  John F Kennedy Intl | New York    | JFK         | Orly                | Paris            | ORY
+--  John F Kennedy Intl | New York    | JFK         | Orly                | Paris            | ORY
+--  John F Kennedy Intl | New York    | JFK         | Orly                | Paris            | ORY
+-- (14 rows)
+
+SELECT 
+	count(*)
+from airports AS origin_airports 
+JOIN routes 
+on origin_airports.iata_faa = routes.origin_code 
+JOIN airports as destination_airports
+on destination_airports.iata_faa = routes.dest_code
+where destination_airports.city = 'Paris' 
+and origin_airports.city = 'New York'
+group by origin_airports.city;
+
+-- count 
+-- -------
+--     14
+-- (1 row)
